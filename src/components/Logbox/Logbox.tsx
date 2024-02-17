9757b4ccd5d234c8ed04472062652ab862b1be38
Fugashu <25103129+Fugashu@users.noreply.github.com>
2024-02-17 13:37:24 +0100

    fix: logbox

--- src/components/Logbox/Logbox.tsx
+++ src/components/Logbox/Logbox.tsx
@@ -1,18 +1,11 @@
 import React, { useEffect, useRef } from "react";
 
 export default function Logbox({ messages }) {
-  const messagesEndRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [messages]);
-
   return (
     <div className="log-box">
       {messages.map((message: string, index: number) => (
         <div key={index}>{message}</div>
       ))}
-      <div ref={messagesEndRef} />
     </div>
   );
 }
