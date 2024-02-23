79b3f4cbddae4aa8bc9dc0fa453d7f07b2e88026
Fugashu <25103129+Fugashu@users.noreply.github.com>
2024-02-23 09:00:36 +0100

    fix: tailwind config paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,10 @@ import { nextui } from "@nextui-org/react";
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
 
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
